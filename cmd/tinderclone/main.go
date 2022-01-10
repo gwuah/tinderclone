c4296e895967d0d2f0363fd6c4ebba048a22dc1e
gwuah <griffithawuah15@gmail.com>
2022-01-10 00:00:37 +0000

    init project

---
+++ cmd/tinderclone/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+func main() {
+
+	decimal.NewFromString("136.02")
+	// internal.griff()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gwuah/tinderclone
+
+go 1.17
+
+require github.com/shopspring/decimal v1.3.1
