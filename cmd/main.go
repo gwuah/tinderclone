7590eec1a5026878d48410f15c686e1839ce6ee0
dibrinsofor <64705019+dibrinsofor@users.noreply.github.com>
2022-01-16 00:48:50 +0100

    removed comments

--- cmd/main.go
+++ cmd/main.go
@@ -15,5 +15,5 @@ func main() {
 	r.GET("/healthcheck", handlers.HealthGet())
 
 	r.Use(cors.New(config))
-	r.Run(":6969") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":6969")
 }
