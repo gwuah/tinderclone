e3f0b3252cba1ccdff75f4cf3f686600fec1cc47
gwuah <griffithawuah15@gmail.com>
2022-01-25 20:11:03 +0000

    cleanup on exit

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gwuah/tinderclone/internal/core/config"
@@ -32,12 +34,33 @@ func main() {
 	workers := q.RegisterJobs([]queue.JobWorker{})
 	go workers.Start()
 
-	r := gin.Default()
+	httpEngine := gin.Default()
 
-	r.Use(middlewares.Cors())
-	r.GET("/", handlers.HealthGet)
-	// remove this or?
-	r.GET("/healthCheck", handlers.HealthGet)
-	r.POST("/createAccount", handlers.CreateAccountPost(db))
-	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	httpEngine.Use(middlewares.Cors())
+	httpEngine.GET("/", handlers.HealthGet)
+	httpEngine.GET("/healthCheck", handlers.HealthGet)
+	httpEngine.POST("/createAccount", handlers.CreateAccountPost(db))
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
+		Handler: httpEngine,
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		if err := server.Close(); err != nil {
+			log.Println("failed To ShutDown Server", err)
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server Closed After Interruption")
+		} else {
+			log.Println("Unexpected Server Shutdown. err:", err)
+		}
+	}
 }
