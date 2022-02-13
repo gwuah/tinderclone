923cc7cf111578663761d27a32de1292b87e0aae
gwuah <griffithawuah15@gmail.com>
2022-02-13 14:10:12 +0000

    fix gorm/pg related bugs

--- internal/migrations/20220201094706_init_user_table.sql
+++ internal/migrations/20220201094706_init_user_table.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS users 
 (
-	id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-	phone_number VARCHAR ( 50 ) NOT NULL,
-	otp VARCHAR NOT NULL,
-	created_at TIMESTAMP NOT NULL
+	id 				UUID 		PRIMARY KEY DEFAULT gen_random_uuid(),	
+	phone_number 	VARCHAR(50) NOT NULL,
+	otp 			VARCHAR 	NOT NULL,
+	created_at 		TIMESTAMP 	NOT NULL
 );
 
 
