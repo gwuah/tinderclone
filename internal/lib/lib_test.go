9a819385fe29e0ffa270f9d0f6d9520e168050b2
gwuah <griffithawuah15@gmail.com>
2022-02-15 20:20:07 +0000

    test send sms

--- internal/lib/lib_test.go
+++ internal/lib/lib_test.go
@@ -11,3 +11,13 @@ func TestGenerateOTP(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 5, len(otp))
 }
+
+// func TestSendSMS(t *testing.T) {
+// 	sms, err := NewSMS("Tinder Clone", os.Getenv("SMS_API_KEY"))
+// 	assert.NoError(t, err)
+
+// 	_, err := sms.SendTextMessage("0205428210", "testing")
+
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, 5, len(otp))
+// }
