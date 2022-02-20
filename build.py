a9f6bf7755b427fc0ea65e11292c545fa8abfdbe
dibrinsofor <dibrinsofor@gmail.com>
2022-02-20 14:17:41 +0100

    modified cli output

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ cmd = str(sys.argv[1])
 
 def runMigration(cmd):
     try:
-        print("Running sweet migrations...")
+        print("Running migrations...")
         time.sleep(3)
         os.system('cmd /c "goose -dir ./internal/migrations postgres "user={} password={} dbname={} sslmode=disable" {}"'.format(os.getenv("DB_USER"), os.getenv("DB_PASS"), os.getenv("DB_NAME"), cmd))
     except BaseException as error:
