f3c366d1d7956302b24824f52d8343e7a3bf9c24
dibrinsofor <dibrinsofor@gmail.com>
2022-03-01 11:08:32 +0100

    changed key to country_code

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID           string    `json:"id" gorm:"default:gen_random_uuid()"`
-	CountryCode  string    `json:"code"`
+	CountryCode  string    `json:"country_code"`
 	PhoneNumber  string    `json:"phone_number"`
 	OTP          string    `json:"otp"`
 	CreatedAt    time.Time `json:"created_at" sql:"type:timestamp without time zone" `
