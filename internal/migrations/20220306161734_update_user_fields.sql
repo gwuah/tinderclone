41576c295b16cec7d675ea0bc1f968262aa53a59
Kwaku Biney <kwakubiney@Kwakus-MacBook-Pro.local>
2022-03-16 13:04:54 +0000

    merged migration files

--- internal/migrations/20220306161734_update_user_fields.sql
+++ internal/migrations/20220306161734_update_user_fields.sql
@@ -1,11 +1,13 @@
 -- +goose Up
+CREATE EXTENSION postgis;
 alter table users
 add first_name  VARCHAR,
 add dob         DATE,
-add location    VARCHAR ;
+add location    geography;
 
 -- +goose Down
 alter table users
 drop first_name,
 drop dob,
 drop location;
+DROP EXTENSION IF EXISTS postgis cascade;
--- internal/migrations/20220315155041_alter_location_type.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
-CREATE EXTENSION postgis;
-alter table users
-alter column location type geography
-USING location::geography;;
-
--- +goose Down
-alter table column location type string;
